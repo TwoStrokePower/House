cc4719d2e9e067f447bced14b4afea862ef9e83a
Dblr4ik 123 <egrik2807@mail.ru>
2023-10-17 19:37:47 +0300

    123

--- House/Program.cs
+++ House/Program.cs
@@ -7,15 +7,6 @@ namespace House
     {
         public static void Main()
         {
-            //House building_1 = new House("Здание №1", "Красный", "Кирпич");
-            //building_1.Show();
-            //building_1.MakeFloor(2);
-            //building_1.MakeFlatOnFloor(2, 10, "FlatRoom");
-            //building_1.MakeFlatOnFloor(2, 11, "Flat");
-            //building_1.ShowFlatsOnFloor(2);
-            //building_1.MakeFloor(3);
-            //building_1.GetFloors();
-            //building_1.GetTypeFlat(2, 10);
 
             var newHouse = HouseBuilderClass.GenerateNewHouse("Здание № 1");
             newHouse.AddNewFloor(-1);
